e566fb72f2f487a5d26402572999efadab743ee8
LM Dijkstra <lmd.p0cus@gmail.com>
2013-03-10 22:58:01 +0100

    Sponsors afgerond
    Geupdate

--- includes/functions.php
+++ includes/functions.php
@@ -4742,7 +4742,10 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 		'L_INDEX'			=> $user->lang['FORUM_INDEX'],
 		'L_PORTAL'			=> $user->lang['PORTAL_INDEX'],
 		'L_ONLINE_EXPLAIN'	=> $l_online_time,
-
+		// GUMBO ADDED BEGIN
+		'U_SPONSORS'			=> append_sid("{$phpbb_root_path}sponsors.$phpEx"),
+		'L_SPONSORS'			=> $user->lang['SPONSOR'],
+		// GUMBO ADDED END
 		'U_PRIVATEMSGS'			=> append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=pm&amp;folder=inbox'),
 		'U_RETURN_INBOX'		=> append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=pm&amp;folder=inbox'),
 		'U_POPUP_PM'			=> append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=pm&amp;mode=popup'),
@@ -4750,9 +4753,6 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 		'U_MEMBERLIST'			=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
 		'U_VIEWONLINE'			=> ($auth->acl_gets('u_viewprofile', 'a_user', 'a_useradd', 'a_userdel')) ? append_sid("{$phpbb_root_path}viewonline.$phpEx") : '',
 		'U_LOGIN_LOGOUT'		=> $u_login_logout,
-		// GUMBO ADDED BEGIN
-		'U_SPONSORS'			=> append_sid("{$phpbb_root_path}sponsors.$phpEx"),
-		// GUMBO ADDED END
 		'U_INDEX'				=> append_sid("{$phpbb_root_path}index.$phpEx"),
 		'P_INDEX'				=> append_sid("{$phpbb_root_path}portal.$phpEx"),
 		'U_SEARCH'				=> append_sid("{$phpbb_root_path}search.$phpEx"),
