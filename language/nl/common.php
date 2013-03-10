723544b605b2350b1ba036916fb6f7f961c605e4
LM Dijkstra <lmd.p0cus@gmail.com>
2013-03-10 23:00:40 +0100

    Taal update voor nederlanders
    Jopie!

--- language/nl/common.php
+++ language/nl/common.php
@@ -40,6 +40,9 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
+	// GUMBO BEGIN
+	'SPONSOR'			=> 'Sponsors',
+	//GUMBO END
 	'TRANSLATION_INFO'	=> '<a href="http://www.phpbb.nl">phpBB.nl Vertaling</a>', // Copyright mag verwijderd worden
 	'DIRECTION'			=> 'ltr',
 	'DATE_FORMAT'		=> '|d M Y|',	// 01 Jan 2007 (with Relative days enabled)
