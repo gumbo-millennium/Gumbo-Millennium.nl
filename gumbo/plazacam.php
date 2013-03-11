06045503f2e101c62d8ce77b44fc71ff5dc30d51
LM Dijkstra <lmd.p0cus@gmail.com>
2013-03-12 00:18:40 +0100

    Iets met juiste paden ./../
    /./

--- gumbo/plazacam.php
+++ gumbo/plazacam.php
@@ -1,6 +1,6 @@
 <?php
 define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './..';
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 include($phpbb_root_path . 'includes/bbcode.' . $phpEx);
