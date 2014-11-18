fd17a9a79cbd78af088f17fc34f7fbe78a261fd9
Sven <sven@plplpl.nl>
2014-11-18 22:02:19 +0100

    HEEL VEEL DINGEN

---
+++ gumbo/pictures.php
@@ -0,0 +1,65 @@
+<?php
+	/**
+	*
+	* @package phpBB3
+	* @version $Id$
+	* @copyright (c) 2005 phpBB Group
+	* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+	*
+	*/
+
+	/**
+	*/
+
+	/**
+	* @ignore
+	*/
+	define('IN_PHPBB', true);
+	$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../';
+	$phpEx = substr(strrchr(__FILE__, '.'), 1);
+	include($phpbb_root_path . 'common.' . $phpEx);
+	include($phpbb_root_path . 'includes/bbcode.' . $phpEx);
+	include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+
+	// Start session management
+	$user->session_begin();
+	$auth->acl($user->data);
+	$user->setup('viewforum');
+	
+	// Get the page content and title
+	$sql = 'SELECT * FROM ' . POSTS_TABLE . ' AS p WHERE post_id = 1256';
+
+	$result = $db->sql_query_limit($sql, 1);
+	$row = $db->sql_fetchrow($result);
+	
+	$options = 	($row['enable_bbcode'] ? OPTION_FLAG_BBCODE : 0) +
+				($row['enable_smilies'] ? OPTION_FLAG_SMILIES : 0) + 
+				($row['enable_magic_url'] ? OPTION_FLAG_LINKS : 0);
+	
+	$message = generate_text_for_display($row['post_text'], $row['bbcode_uid'], $row['bbcode_bitfield'], $options);
+	
+	$template->assign_var('MESSAGE_TEXT', $message);
+	
+	$db->sql_freeresult($result);
+	
+	// Add the title/breadcrumbs bar
+	$template->assign_vars(array(
+		'PAGE_TITLE_BOX_HIDE'	=> false
+	));
+	
+	// Set the breadcrumbs
+	$template->assign_block_vars('navlinks', array(
+		'FORUM_NAME'		=> $user->lang['PICTURES'],
+		'U_VIEW_FORUM'		=> append_sid("{$phpbb_root_path}gumbo/pictures.$phpEx" )) //The path to the custom file relative to the phpbb root path.            
+	);
+	
+	// Output page
+	page_header($user->lang['PICTURES']);
+	
+	$template->set_filenames(array(
+		'body' => 'under_construction.html')
+	);
+
+	page_footer();
+
+?>
